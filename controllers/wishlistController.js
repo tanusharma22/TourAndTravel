494178f272aed9ebe85b8edff7162f1e323ebbae
Tanu Sharma <mohitsharma@Mohits-MacBook-Air.local>
2023-12-16 20:13:28 +0530

    wish list error corrected

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,14 +1,29 @@
-const wishlist =require('../model/wishlist.model')
-const wishlistHandler=async (req,res)=>{
-    const newWishlist= new  wishlist(req.body);
-    try{
-        const savedWishlist=await newWishlist.save();
+const Wishlist=require("../model/wishlist.model");
+const createWishlistHandler=async (req, res) => {
+    const newWishlist = new Wishlist(req.body);
+    try {
+        const savedWishlist = await newWishlist.save();
         res.status(201).json(savedWishlist);
-
-    }
-    catch(err){
-        console.log(err)
-        res.status(500).json({message:"failed to create wishlist"})
+    }catch(err){
+        res.status(500).json({ message: "failed to create wishlist" })
     }
 }
-module.exports=wishlistHandler
+const deleteWishlistHandler=async (req, res) => {
+    try{
+        await Wishlist.findByIdAndDelete(req.params.id);
+        res.json({ message: "Hotel Deleted From Wishlist"})
+    }catch(err){
+        res.status(500).json({ message: "Could not delete hotel from wishlist" })
+    }
+}
+
+const getWishlistHandler=async (req, res) => {
+    try{
+        const wishlist = await Wishlist.find({});
+        wishlist ? res.json(wishlist) : res.json({ message: "No items found in the wishlist"})
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+}
+module.exports={createWishlistHandler,deleteWishlistHandler,getWishlistHandler};
--- routes/wishlist.router.js
+++ routes/wishlist.router.js
@@ -1,33 +1,15 @@
-const express=require('express')
-const router= express.Router();
-const wishlist =require('../model/wishlist.model')
-const verifyuser=require('../middleware/verifyuser')
-const wishlistHandler= require('../controllers/wishlistController')
-
+const express = require('express');
+const router=express.Router();
+const wishlistController=require("../controllers/wishlistController")
+const {createWishlistHandler,deleteWishlistHandler,getWishlistHandler}=wishlistController;
+const verifyUser=require("../middleware/verifyuser")
+ 
 router.route("/")
-    .post(verifyuser,wishlistHandler)
-
-    router.route("/:id")
-        .delete(verifyuser,async (req,res) =>{
-            try{
-                await wishlist.findByIdAndDelete(req.params.id);
-                res.json({ message: "Hotel Deleted From Wishlist"})
-            }catch(err){
-                res.status(500).json({ message: "Could not delete hotel from wishlist" })
-            }
-        })
-
+    .post(verifyUser,createWishlistHandler)
+    
+router.route("/:id")
+    .delete(verifyUser,deleteWishlistHandler)
+    
+router.route("/")
+    .get(verifyUser,getWishlistHandler)
     module.exports=router;
-
-    router.route("/")
-        .get(verifyuser,
-            async (req, res) => {
-                try{
-                    const Wishlist = await wishlist.find({});
-                    Wishlist ? res.json(Wishlist) : res.json({ message: "No items found in the wishlist"})
-                }catch(err){
-                    console.log(err)
-                    res.status(500).json(err)
-                }
-            }
-        )
